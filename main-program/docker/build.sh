92f8455f1b506ee0e99db003ecfbdf2479739df6
milak <milak@milak>
2017-06-10 23:06:42 +0200

    Add configuration file

--- main-program/docker/build.sh
+++ main-program/docker/build.sh
@@ -7,7 +7,7 @@ go get -u github.com/google/uuid
 go get -u github.com/milak/mmq
 go get -u github.com/milak/tools
 echo "compiling..."
-#go build src/github.com/milak/mmq/mmq/mmq.go
+go build src/github.com/milak/mmq/mmq/mmq.go
 #sudo docker build --rm -f docker/Dockerfile -t magicmq .
 #rm docker/magicmq
 echo "compile successful"
