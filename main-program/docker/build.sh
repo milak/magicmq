a96801cef88ca6de5e0e20991f817f8c069fd0cf
milak <milak@milak>
2017-06-10 23:09:33 +0200

    Change build.sh

--- main-program/docker/build.sh
+++ main-program/docker/build.sh
@@ -1,13 +1,13 @@
 echo "building..."
-export CGO_ENABLED=0
 mkdir src
+cd src
 export GOPATH=/src
 echo "getting sources..."
 go get -u github.com/google/uuid
 go get -u github.com/milak/mmq
 go get -u github.com/milak/tools
+ls
 echo "compiling..."
-go build src/github.com/milak/mmq/mmq/mmq.go
-#sudo docker build --rm -f docker/Dockerfile -t magicmq .
-#rm docker/magicmq
+export CGO_ENABLED=0
+go build github.com/milak/mmq/mmq/mmq.go
 echo "compile successful"
