d1fb30c34a8c25b64c55378c961268aa311e16d5
milak <milak@milak>
2017-06-10 23:02:17 +0200

    It will work ...

--- main-program/docker/build.sh
+++ main-program/docker/build.sh
@@ -1,11 +1,13 @@
+echo "installing go..."
+apt-get install golang
 echo "building..."
 export CGO_ENABLED=0
 mkdir src
-export GOROOT=src
+export GOPATH=/src
 echo "getting sources..."
-#go get -u github.com/google/uuid
-#go get -u github.com/milak/mmq
-#go get -u github.com/milak/tools
+go get -u github.com/google/uuid
+go get -u github.com/milak/mmq
+go get -u github.com/milak/tools
 echo "compiling..."
 #go build src/github.com/milak/mmq/mmq/mmq.go
 #sudo docker build --rm -f docker/Dockerfile -t magicmq .
